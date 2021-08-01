c9d793c5067dc73957d2324454ab407851e1c3e8
Julian <jak@condat.de>
2021-08-01 10:48:15 +0200

    Added data for integration tests

--- src/main/resources/db/migration/V1__Create_short_url_table.sql
+++ src/main/resources/db/migration/V1__Create_short_url_table.sql
@@ -2,9 +2,9 @@ CREATE TABLE short_url
 (
     id   int AUTO_INCREMENT NOT NULL,
     long_url text NOT NULL,
-    short_url varchar(255) NOT NULL,
+    short_path varchar(10) NOT NULL,
     hit_counter int NOT NULL DEFAULT 0,
     create_counter int NOT NULL DEFAULT 0,
     PRIMARY KEY (id),
-    UNIQUE (short_url)
+    UNIQUE (short_path)
 );
---
+++ src/test/resources/url-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO short_url (id, long_url, short_path, hit_counter, create_counter)
+values (1, 'https://www.axelspringer.com/en/brands/rolling-stone', 'Aj8L3P', 5, 8),
+       (2, 'http://www.axelspringer.com/en/brands/rolling-stone', 'K1fmF8', 0, 1);
